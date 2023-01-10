458c0a4528a85d60ab4e9ea4214e74350ddf0a6a
jayeong <940@educere.biz>
2023-01-11 00:21:14 +0900

    [A] Add Data

---
+++ src/data/questions.js
@@ -0,0 +1,42 @@
+// 질문지 더미 데이터
+export const questions = [
+  {
+    title: " 첫번째 질문입니다 여기 내용 필요해요 ? ",
+    options: [
+      {
+        index: "A",
+        description: " 나는 1번이 좋아 ",
+      },
+      {
+        index: "B",
+        description: " 나는 2번이 좋아 ",
+      },
+    ],
+  },
+  {
+    title: " 두번째 질문입니다 여기 내용 필요해요 ? ",
+    options: [
+      {
+        index: "A",
+        description: " 나는 1번이 좋아 ",
+      },
+      {
+        index: "B",
+        description: " 나는 2번이 좋아 ",
+      },
+    ],
+  },
+  {
+    title: " 세번째 질문입니다 여기 내용 필요해요 ? ",
+    options: [
+      {
+        index: "A",
+        description: " 나는 1번이 좋아 ",
+      },
+      {
+        index: "B",
+        description: " 나는 2번이 좋아 ",
+      },
+    ],
+  },
+];
